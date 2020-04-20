c1cf01c5fbf5abc8e9bbbf9c0e1253ad7a99eb74
mohieddinne <gdidamohieddinnesimplon@gmail.com>
2020-04-20 16:06:28 +0100

    add

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+        I AM A PARAGRAPH
+        </p>
+        <a
+          className="google"
+          href="https://www.google.com/?hl=fr"
+          
+          
+        >
+         Visit GooGle
+        </a>
+        <ul>
+          <li>veille: best formateur </li>
+          <li>workshop react js</li>
+          <li>defi react </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
